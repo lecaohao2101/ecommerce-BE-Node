107efa7c63a6b5e32658ca4a10cfe0d83bf4c63e
Lê Cao Hào <95235601+lecaohao2101@users.noreply.github.com>
2024-02-11 20:46:35 +0700

    Delete src directory

--- src/routes/access/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const accessController = require('../../controllers/access.controller');
-const router = express.Router();
-
-
-// sign up
-router.post('/shop/signup', accessController.signUp)
-
-module.exports = router;
